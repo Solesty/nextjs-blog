2da13007d9889f2e33837349b5d808c63680749a
ABOYEJI SOLOMON ADELEKE <solesty7@gmail.com>
2020-05-22 17:23:03 +0100

    Pre-rendering and Data Fetching - Pre-Rendering (limited)

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,55 @@ import Layout, { siteTitle } from '../components/layout'
 import Alert from '../components/alert'
 import utilStyles from '../styles/utils.module.css'
 
-export default function Home() {
+import { getSortedPostsData } from '../lib/posts'
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>I am a Liquid Metal!</p>
-        <Alert type="error">
-          I am Indaboski
-        </Alert>
+      <section className={utilStyles.headingMd} >...</section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg} >Blog</h2>
+        <ul className={utilStyles.list}>
+          {
+            allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                {title}
+                <br />
+                {id}
+                <br />
+                {date}
+              </li>
+            ))
+          }
+        </ul>
       </section>
     </Layout>
   )
+}
+
+// This runs only at run time in development
+// It runs only at build time in production
+export async function getStaticProps() {
+
+  // Instead of the file system,
+  // fetch post data from an external API endpoint
+  //
+  // const res = await fetch('..')
+  // return res.json()
+
+
+  // Instead of the file system,
+  // fetch post data from a database
+  // return databaseClient.query('SELECT posts...')
+
+  // Read From File System
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
